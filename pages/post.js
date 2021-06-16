4b720a9e1c5d52ffe27b18b38e5e95a9fdbb402c
miriam cordova ortiz <manuelfrankterancampi@gmail.com>
2021-06-16 13:31:59 -0500

    agrege imgurl a profile

--- pages/post.js
+++ pages/post.js
@@ -1,6 +1,7 @@
 import Layout from '../components/Layout';
 import { useRouter } from 'next/router';
-import { posts } from '../profile';
+import { posts, imageURL, content } from '../profile';
+
 
 
 const Post = () => {
@@ -8,16 +9,14 @@ const Post = () => {
 const router = useRouter();
 
 const currentPost = posts.filter(
-	(post) => post.title === router.query.title,
-	(imageURL) => post.imageURL === router.query.imageURL,
-	(content) => post.content === router.query.content
+	(post) => post.title === router.query.title
 )[0];
 
 return (
 		<Layout title={router.query.title} footer={false}>
 	 	<div className="text-center">
 	 		<img 
-	 		src={currentPost.imageURL} 
+	 		src={currentPost.imageURL}
 	 		alt="" 
 	 		className="img-fluid" 
 	 		style={{width: '50%'}}
